5507e563f37ce3a6bf37f3147eacbc9bd56044da
atroncoso2 <atroncoso2@uc.cl>
2020-09-20 20:45:30 -0300

    pagina(/): prueba conexion db

--- Sites/index.php
+++ Sites/index.php
@@ -2,4 +2,14 @@ Hello World!
 
 <?php
     require('./config/conection.php')
+
+    $query = "SELECT * FROM facilities;";
+    $result = $db -> prepare($query);
+    $result -> execute();
+    $facilities = $result -> fetchAll();
+?>
+<?php
+    foreach ($facilities as $f) {
+        echo "f"
+    }
 ?>
