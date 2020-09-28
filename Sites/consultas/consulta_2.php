824b2b34f89f59c55bfe24ee24a3860afedf9842
jordonezp <jordonez@uc.cl>
2020-09-28 16:52:09 -0300

    Modificación en consulta 2

--- Sites/consultas/consulta_2.php
+++ Sites/consultas/consulta_2.php
@@ -3,9 +3,7 @@ require('../config/conection.php');
 
 $input_2 = strtoupper($_POST["input_2"]);
 
-<?php
-    echo $input_2
-?>
+echo $input_2;
 
 $query = "SELECT facilities.boss_rut FROM (SELECT pid FROM ports WHERE UPPER(name) LIKE '%$input_2%') AS puertos, facilities;";
 $result = $db -> prepare($query);
