9312f7cf07df3463aec24607690b2e5b05e10d29
jordonezp <jordonez@uc.cl>
2020-09-29 09:26:00 -0300

    Realización consultas 4, 5 y 6. Eliminación consultas_generales.php. Limpieza index.php

--- Sites/index.php
+++ Sites/index.php
@@ -7,11 +7,8 @@
     $facilities = $result -> fetchAll();
 ?>
 
-<!--todo: Verificar que el procesamiento de datos esté bien-->
-<!--todo: Verificar resultados de consultas 4, 5 y 6-->
-<!--todo: Discutir que no haya dependencia funcional relación FacilityHistoryEntries-->
-<!--todo: -->
-<!--todo: -->
+<!--todo: Modificar las consultas de php-->
+<!--todo: Verificar que el procesamiento de datos está bien-->
 
 <!---->
 <!---->
@@ -61,45 +58,3 @@
                                                                                     name="input_6_2">. <br>
     <input type="submit" value="Buscar">
 </form>
-
-<!---->
-<!---->
-<!---->
-<!---->
-<!---->
-<!---->
-<!---->
-<!---->
-<!---->
-
-    <h1>Hello World!!!</h1>
-<br><br>
-<form action="consultas/consultas_generales.php" method="post">
-    Input: 
-    <input type="text" name="input">
-    <br>
-    <input type="submit" name="submit" value="Submit"> 
-</form>
-<br>
-<br>
-<table>
-    <tr>
-        <th>fid</th>
-        <th>type</th>
-        <th>capacity</th>
-        <th>boss_rut</th>
-        <th>pid</th>
-    </tr>
-<?php
-    foreach ($facilities as $f) {
-        echo 
-        "<tr>
-            <td>$f[0]</td>
-            <td>$f[1]</td>
-            <td>$f[2]</td>
-            <td>$f[3]</td>
-            <td>$f[4]</td>
-        </tr>"
-        ;
-    }
-?>
