73dac893a3da36f79f42b2a35e14e54a739cb761
atroncoso2 <atroncoso2@uc.cl>
2020-10-21 15:21:42 -0300

    proc_almacenados

---
+++ Entrega3/proc_alm/filter_facilities_by_available_dates.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE FUNCTION
+filter_facilities_by_available_dates(t1 timestamp, t2 timestamp)
+RETURNS TABLE (fid int, type varchar(100), capacity int, boss_rut varchar(50), pid2 int) AS $$
+BEGIN
+RETURN QUERY EXECUTE 'SELECT * FROM facilities WHERE facilities.pid = $1'USING pid;
+RETURN;
+END
+$$
+language plpgsql;
