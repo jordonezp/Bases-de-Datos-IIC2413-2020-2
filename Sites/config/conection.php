0ea7eaff6147bd4ba472238919cb211afb5ecd4a
jordonezp <jordonez@uc.cl>
2020-10-21 12:35:15 -0300

    consulta db par

--- Sites/config/conection.php
+++ Sites/config/conection.php
@@ -4,7 +4,7 @@
     require('dataimp.php');
     require('datap.php');
     # Se crea la instancia de PDO
-    $dbimp = new PDO("pgsql:dbname=$databaseNam eimp;host=localhost;port=5432;user=$userimp;password=$passwordimp");
+    $dbimp = new PDO("pgsql:dbname=$databaseNameimp;host=localhost;port=5432;user=$userimp;password=$passwordimp");
     $dbp = new PDO("pgsql:dbname=$databaseNamep;host=localhost;port=5432;user=$userp;password=$passwordp");
   } catch (Exception $e) {
     echo "No se pudo conectar a la base de datos: $e";
