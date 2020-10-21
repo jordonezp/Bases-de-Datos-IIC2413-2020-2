53880e9fcb1ab56732a50b2531ba61f889101b4f
jordonezp <jordonez@uc.cl>
2020-10-21 19:15:13 -0300

    se junto codigo grupos impar y par

--- Sites/consultas/crear_cuenta.php
+++ Sites/consultas/crear_cuenta.php
@@ -5,7 +5,7 @@
 
 <?php
     #Llama a conexión, crea el objeto PDO y obtiene la variable $db
-    require("../config/conexion.php");
+    require("../config/conection.php");
 
     $pasaporte = $_POST["pasaporte"];
     $pass = $_POST["pass"];
