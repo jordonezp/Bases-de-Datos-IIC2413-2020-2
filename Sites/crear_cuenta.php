6225efc6d17eaed4dbfc963f6e7f98dac782b0a9
Nicolas Court <ncourt@uc.cl>
2020-10-22 17:42:13 -0300

    Minor_Changes_v2

--- Sites/crear_cuenta.php
+++ Sites/crear_cuenta.php
@@ -9,13 +9,10 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
 
 
-<body>
-  <h1 class="title">Creador de cuenta</h1>
-  <p class="subtitle">Ingresa tu informacion.</p>
-<body/>
-
   <br/>
   <div class="container is-max-desktop">
+  <h1 class="title">Creador de cuenta</h1>
+  <p class="subtitle">Ingresa tu informacion.</p>
         <form align="center" action="consultas/crear_cuenta.php" method="post">
             pasaporte:
             <input class="input is-rounded" style="width: 33%;" type="text" name="pasaporte">
