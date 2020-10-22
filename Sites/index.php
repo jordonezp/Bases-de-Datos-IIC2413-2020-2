79cd73b4255545884d410e9545b293690ed8c184
Nicolas Court <ncourt@uc.cl>
2020-10-22 16:14:19 -0300

    Accounts_v10

--- Sites/index.php
+++ Sites/index.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
+
 
 <?php
 session_start();
@@ -13,3 +13,4 @@ echo $_SESSION['name'];
 ?>
 <?php include('templates/header.html');   ?>
 <?php include('navbar.php'); ?>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
