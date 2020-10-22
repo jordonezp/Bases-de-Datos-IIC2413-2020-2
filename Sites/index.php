700da3117f2ab2f195d42a33bab48fc73bc71b40
Nicolas Court <ncourt@uc.cl>
2020-10-22 17:16:23 -0300

    Index

--- Sites/index.php
+++ Sites/index.php
@@ -6,8 +6,6 @@ if (!isset($_SESSION['name']))
   $_SESSION['name'] = "";
   $_SESSION['pasaporte'] = "";
 }
-
-echo $_SESSION['name'];
 ?>
 <?php include('templates/header.html');   ?>
 <?php include('navbar.php'); ?>
