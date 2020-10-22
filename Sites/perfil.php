8cfb2770cb2521cab1dd509a585628282bf0417c
jordonezp <jordonez@uc.cl>
2020-10-21 21:19:31 -0300

    creación y avance archivos respuesta_permiso.php y respuesta_ocupacion.php

--- Sites/perfil.php
+++ Sites/perfil.php
@@ -10,40 +10,58 @@ $edad = $_POST["edad"];
 $sexo = $_POST["sexo"];
 $nacio = $_POST["nacio"];
 
-
-//CAPITAN
-
-$query = "SELECT personal.patente FROM personal 
+// querys para clasificar tipo de persona
+$query = "SELECT personal.pasaporte FROM personal 
             WHERE personal.capitan = True AND personal.pasaporte = '$pasaporte';";
 $result = $dbp -> prepare($query);
 $result -> execute();
-$patente_capitan = $result -> fetchAll();
+$capitan = $result -> fetchAll();
 
-$query = "SELECT buque.bnombre FROM buque 
-            WHERE buque.patente 
-            IN (SELECT personal.patente AS patente FROM personal 
-            WHERE personal.capitan = True AND personal.pasaporte = '$pasaporte');";
+$query = "SELECT f.boss_rut FROM facilities f
+            WHERE f.boss_rut = '$pasaporte';";
 $result = $dbp -> prepare($query);
 $result -> execute();
-$bnombre_capitan = $result -> fetchAll();
+$jefe = $result -> fetchAll();
 
-$query = "SELECT naviera.nnombre FROM naviera 
-            WHERE naviera.nid IN (SELECT buque.nid FROM buque 
-            WHERE buque.patente IN (SELECT personal.patente AS patente FROM personal 
-            WHERE personal.capitan = True AND personal.pasaporte = '$pasaporte'));";
-$result = $dbp -> prepare($query);
-$result -> execute();
-$naviera_capitan = $result -> fetchAll();
 
-$query = "SELECT puerto.punombre FROM puerto 
-            WHERE puerto.puid IN (SELECT historialatraque.puid AS puertos FROM historialatraque 
-            WHERE historialatraque.patente IN (SELECT personal.patente FROM personal 
-            WHERE personal.capitan = True AND personal.pasaporte = '$pasaporte') 
-            GROUP BY historialatraque.fecha, historialatraque.puid 
-            ORDER BY historialatraque.fecha DESC LIMIT 5);";
-$result = $dbp -> prepare($query);
-$result -> execute();
-$puertos_capitan = $result -> fetchAll();
+if (sizeof($capitan) == 1) {
+    //CAPITAN
+
+    $query = "SELECT personal.patente FROM personal 
+    WHERE personal.capitan = True AND personal.pasaporte = '$pasaporte';";
+    $result = $dbp -> prepare($query);
+    $result -> execute();
+    $patente_capitan = $result -> fetchAll();
+
+    $query = "SELECT buque.bnombre FROM buque 
+    WHERE buque.patente 
+    IN (SELECT personal.patente AS patente FROM personal 
+    WHERE personal.capitan = True AND personal.pasaporte = '$pasaporte');";
+    $result = $dbp -> prepare($query);
+    $result -> execute();
+    $bnombre_capitan = $result -> fetchAll();
+
+    $query = "SELECT naviera.nnombre FROM naviera 
+    WHERE naviera.nid IN (SELECT buque.nid FROM buque 
+    WHERE buque.patente IN (SELECT personal.patente AS patente FROM personal 
+    WHERE personal.capitan = True AND personal.pasaporte = '$pasaporte'));";
+    $result = $dbp -> prepare($query);
+    $result -> execute();
+    $naviera_capitan = $result -> fetchAll();
+
+    $query = "SELECT puerto.punombre FROM puerto 
+    WHERE puerto.puid IN (SELECT historialatraque.puid AS puertos FROM historialatraque 
+    WHERE historialatraque.patente IN (SELECT personal.patente FROM personal 
+    WHERE personal.capitan = True AND personal.pasaporte = '$pasaporte') 
+    GROUP BY historialatraque.fecha, historialatraque.puid 
+    ORDER BY historialatraque.fecha DESC LIMIT 5);";
+    $result = $dbp -> prepare($query);
+    $result -> execute();
+    $puertos_capitan = $result -> fetchAll();
+
+} 
+
+
 
 
 
@@ -59,7 +77,19 @@ $query = "SELECT s.fid FROM shipyards s, facilities fa
             WHERE fa.boss_rut ='$pasaporte' AND s.fid = fa.fid;";
 $result = $dbp -> prepare($query);
 $result -> execute();
-$tipo_instalacion_jefe = $result -> fetchAll();
+$tipo_jefe = $result -> fetchAll();
+
+if (sizeof($tipo_jefe) == 1) {
+    echo "Jefe de un Shipyard";
+    $tipo_inst_jefe = "Shipyard";
+    
+} elseif (sizeof($nav) == 0) {
+    echo "Jefe de un Dock";
+    $tipo_inst_jefe = "Dock";
+
+} else {
+    echo "Hay 2 ????";
+}
 
 
 ?>
