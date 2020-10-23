6a0beaf969b5ad745a77a740721c57a67099e624
Nicolas Court <ncourt@uc.cl>
2020-10-22 21:43:01 -0300

    Profile_v7

--- Sites/consultas/logout.php
+++ Sites/consultas/logout.php
@@ -5,7 +5,7 @@ $_SESSION["clave"] = "";
 <?php include('../templates/header.html');   ?>
 <?php include('../navbar.php'); ?>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
-
+<br/><br/><br/><br/>
 <div class="container is-max-desktop">
   <h2 class="title">Log-Out Successfull</h2>
 </div>
