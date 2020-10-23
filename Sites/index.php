97f66098c79b739e46bbc3ba482b977349e626eb
Nicolas Court <ncourt@uc.cl>
2020-10-22 21:47:05 -0300

    Profile_v8

--- Sites/index.php
+++ Sites/index.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['name']))
 <?php include('templates/header.html');   ?>
 <?php include('navbar.php'); ?>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
-
+<br/><br/><br/>
 
 
 <div class="container is-max-desktop">
