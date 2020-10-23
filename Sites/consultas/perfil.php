dd65bbbe185109c11cfbf1860d5ed2a95c63f010
Nicolas Court <ncourt@uc.cl>
2020-10-22 21:38:44 -0300

    Profile_v5

--- Sites/consultas/perfil.php
+++ Sites/consultas/perfil.php
@@ -1,8 +1,7 @@
 <?php session_start();?>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
 <?php include('../navbar.php'); ?>
-<?php include('templates/header.html');   ?>
-<?php include('navbar.php'); ?>
+<?php include('../templates/header.html');   ?>
 <?php
 require("../config/conection.php");
 
