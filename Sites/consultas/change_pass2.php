aab8f82941d920d15c7770f637f888707863865e
Nicolas Court <ncourt@uc.cl>
2020-10-25 19:15:02 -0300

    Changepass_v11

--- Sites/consultas/change_pass2.php
+++ Sites/consultas/change_pass2.php
@@ -2,7 +2,9 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
 <?php include('../navbar.php'); ?>
 <?php include('../templates/header.html');   ?>
+
 <?php
+require('../config/conection.php');
 $old = $_POST["old"];
 $new = $_POST["new"];
 $pas = $_SESSION["pasaporte"];
