ebb7bcd3318ceb524d386dc50a567ab03990b23e
jordonezp <jordonez@uc.cl>
2020-10-27 10:35:16 -0300

    Filtro navieras.php terminado

--- Sites/navieras.php
+++ Sites/navieras.php
@@ -2,7 +2,13 @@
 
 <?php
 require('./config/conection.php');
-$query = "SELECT * FROM naviera;";
+if (isset($_POST["submit"])) {
+    $nombre = strtoupper($_POST["nombre"]);
+    $query = "SELECT * FROM naviera WHERE UPPER(nnombre) LIKE '%$nombre%';";
+}
+else {
+    $query = "SELECT * FROM naviera;";
+}
 $result = $dbp -> prepare($query);
 $result -> execute();
 $tabla = $result -> fetchAll();
@@ -16,6 +22,15 @@ $tabla = $result -> fetchAll();
   <h1 class="title">Navieras</h1>
     <h2 class="subtitle">Acá podrás revisar las navieras existentes. Haciendo <i>click</i> sobre una naviera podrás
     acceder a los barcos de esta.</h2>
+
+    <!-- Acá va el form para filtrar-->
+    <p> Filtrar por nombre</p>
+    <form action="navieras.php" method="post">
+        <input class="button is-link" type="text" name="nombre" placeholder="Nombre...">
+        <input class="button is-link" type="submit" value="Filtrar" name="submit">
+    </form>
+    </br>
+    </br>
 
   <table>
     <tr>
