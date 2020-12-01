accdee3c6d287c7f4ecd52e679e5c187c9d71793
atroncoso2 <atroncoso2@uc.cl>
2020-12-01 13:44:26 -0300

    nuevo archivo->mensajes recibidos

--- Sites/consultas/consultas_api/recieved_messages.php
+++ Sites/consultas/consultas_api/recieved_messages.php
@@ -28,20 +28,60 @@ function sendGet($url) {
 $url = "https://api-bdd-g-94-81.herokuapp.com/messages";
 
 $response = sendGet($url);
-echo $response;
+// echo $response;
 
 // $json = json_decode($response);
 // echo $json;
 $jsonData = json_decode($response, JSON_INVALID_UTF8_IGNORE);
 // echo $jsonData;
-echo '<pre>';
-var_dump($jsonData);
-echo '</pre>';
+// echo '<pre>';
+// var_dump($jsonData);
+// echo '</pre>';
 
 foreach ($jsonData as $m) {
+    $rec = $m["receptant"];
+    if ($rec === $usuario_id) {
+        echo 
+    }
     echo "\n m[0]: $m[0]";
     echo "\n\n m['date']: ";
     echo $m["date"];
 }
 
 ?>
+
+<table>
+    <tr>
+        <th>date</th>
+        <th>lat</th>
+        <th>long</th>
+        <th>message</th>
+        <th>mid</th>
+        <th>receptant</th>
+        <th>sender</th>
+    </tr>
+
+    <?php
+        foreach ($jsonData as $m) {
+            $rec = $m["receptant"];
+            if ($rec === $usuario_id) {
+                echo "<tr>";
+                echo "<td>$m['date']</td>"
+                echo "<td>$m['lat']</td>"
+                echo "<td>$m['long']</td>"
+                echo "<td>$m['mid']</td>"
+                echo "<td>$m['message']</td>"
+                echo "<td>$m['receptant']</td>"
+                echo "<td>$m['sender']</td>"
+                echo "</tr>";
+            }
+            // echo "\n m[0]: $m[0]";
+            // echo "\n\n m['date']: ";
+            // echo $m["date"];
+        }
+        // foreach ($tabla as $fila) {
+        //     echo "<tr> <td> <a href='consultas/consulta_puertos.php?pid=$fila[0]&name=$fila[1]'> $fila[1] </a> </td> </tr>";
+        // }
+    ?>
+
+</table>
