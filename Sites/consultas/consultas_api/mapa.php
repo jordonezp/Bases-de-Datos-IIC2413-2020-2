1db779ef730f0b7f65ca401ac653a4ce49e1d18a
atroncoso2 <atroncoso2@uc.cl>
2020-12-02 01:13:07 -0300

    mapas comienzo

--- Sites/consultas/consultas_api/mapa.php
+++ Sites/consultas/consultas_api/mapa.php
@@ -15,35 +15,19 @@ $desired = $_GET["desired"];
 $required = $_GET["required"];
 
 $userId = (int) $userId;
-$forbidden_a = preg_split("/[;]+/", $forbidden);
 $desired_a = preg_split("/[;]+/", $desired);
-$required_a = preg_split("/[;]+/", $required);
 
 // echo $userId;
 // echo $forbidden;
 // echo $desired;
 // echo $required;
-
-function sendGet($url) {
-    try{
-        $response = file_get_contents($url);
-
-        if ($response !== false) {
-            return $response;
-        }
-    } catch (Exception $e) {
-        return $e->getMessage();
-    }
-}
 $url = "https://bdd-e5-g9481.herokuapp.com/text-search";
 
 $ch = curl_init($url);
 
 $data = array(
     'userId' => $userId,
-    'forbidden' => $forbidden_a,
     'desired' => $desired_a,
-    'required' => $required_a
 );
 $payload = json_encode($data);
 
@@ -68,27 +52,19 @@ $jsonData = json_decode($result, JSON_INVALID_UTF8_IGNORE);
     <p>En caso de ingresar múltiples términos, separar por ",". </p>
     <br>
     <?php
-    foreach ($forbidden_a as $f) {
-        echo "$f\n";
-    }
-    foreach ($desired_a as $d) {
-        echo "$d\n";
-    }
-    foreach ($required_a as $r) {
-        echo "$r\n";
-    }
     echo "payload: $payload\n";
     ?>
     
     <form align="center" action="mapa.php" method="get">
+        <input type="hidden" name="sender" value=<?php echo $usuario_id ?> />
         <p>Usuario (id): </p>
         <input class="input is-rounded" style="width: 33%;" type="number" name="userId">
-        <p>Prohibido: </p>
-        <input class="input is-rounded" style="width: 33%;" type="text" name="forbidden">
-        <p>Deseado: </p>
+        <p>Palabras Clave: </p>
         <input class="input is-rounded" style="width: 33%;" type="text" name="desired">
-        <p>Requerido: </p>
-        <input class="input is-rounded" style="width: 33%;" type="text" name="required">
+        <p>Fecha inicio: </p>
+        <input class="input is-rounded" style="width: 33%;" type="date" name="start_date">
+        <p>Fecha final: </p>
+        <input class="input is-rounded" style="width: 33%;" type="date" name="end_date">
         <br>
         <br>
         <input class="button is-link" type="submit" value="Buscar Mensajes">
