0437b307d37ec225c1a4f5d1d64d7ecd79ba7a62
atroncoso2 <atroncoso2@uc.cl>
2020-12-02 01:40:00 -0300

    mapas comienzo

--- Sites/consultas/consultas_api/mapa.php
+++ Sites/consultas/consultas_api/mapa.php
@@ -4,6 +4,9 @@
 
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+	integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+	crossorigin=""/>
 
 <?php
 
@@ -100,13 +103,6 @@ $jsonData = json_decode($result, JSON_INVALID_UTF8_IGNORE);
                     $sender =  $m["sender"];
                     echo "<tr><td>$date</td><td>$lat</td><td>$long</td><td>$mid</td><td>$message</td><td>$receptant</td><td>$sender</td></tr>";
                 }
-                // $lat =  $m["lat"];
-                // $long =  $m["long"];
-                // $mid =  $m["mid"];
-                // $message =  $m["message"];
-                // $receptant =  $m["receptant"];
-                // $sender =  $m["sender"];
-                // echo "<tr><td>$date</td><td>$lat</td><td>$long</td><td>$mid</td><td>$message</td><td>$receptant</td><td>$sender</td></tr>";
             }
         ?>
         </tbody>
@@ -114,10 +110,38 @@ $jsonData = json_decode($result, JSON_INVALID_UTF8_IGNORE);
     </table>
     <br>
     <br>
+    <div id="mapid" style="height: 300px"></div>
     <?php
-        echo "start: $start_date\n";
-        echo "end: $end_date\n";
-        echo $start_date < $end_date;
+        // echo "start: $start_date\n";
+        // echo "end: $end_date\n";
+        // echo $start_date < $end_date;
     ?>
 </div>
+<?php 
+    $lat = -33.5;
+    $long = -70.5;
+    $marker_list = [
+        ["lat" => -33.4,
+        "long" => -70.5],
+        ["lat" => -33.6,
+        "long" => -70.5],
+        ["lat" => -33.5,
+        "long" => -70.6],
+    ];
+?>
 
+<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+   integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+   crossorigin=""></script>
+<script>
+    var map = L.map('mapid').setView([<?php echo $lat ?>, <?php echo $long ?>], 10);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    <?php foreach($marker_list as $marker) {
+        echo 
+        'L.marker([' . $marker["lat"] . ',' . $marker["long"] . ']).addTo(map);';
+    } ?>
+</script>
