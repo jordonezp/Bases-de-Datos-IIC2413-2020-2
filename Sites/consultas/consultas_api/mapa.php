e162320476b0b3919baa7e1acc819a9f7369327b
Macarena Gamerre <mfgamerre@uc.cl>
2020-12-07 17:37:54 -0300

    msjes por id marker green3

--- Sites/consultas/consultas_api/mapa.php
+++ Sites/consultas/consultas_api/mapa.php
@@ -163,6 +163,15 @@ $marker_list = []
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
+    var greenIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+        });
+
     <?php foreach($marker_list as $marker) {
         echo 
         'L.marker([' . $marker["lat"] . ',' . $marker["long"] . '], {icon: greenIcon}).addTo(map);';
