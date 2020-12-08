642e1d04f71f4084370eeca2084654dfa5fd9aec
atroncoso2 <atroncoso2@uc.cl>
2020-12-08 17:29:34 -0300

    mapa(mapa): echo detalle de query

--- Sites/consultas/consultas_api/mapa.php
+++ Sites/consultas/consultas_api/mapa.php
@@ -204,7 +204,8 @@ $marker_list2 = [];
     }
     
     ?>
-</div>
+
+<h2 class="title">Resultados</h2>
 
 <table class="table">
         <thead>
@@ -244,6 +245,7 @@ $marker_list2 = [];
             ?>
         </tbody>    
 </table>
+</div>
 
 <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
    integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
