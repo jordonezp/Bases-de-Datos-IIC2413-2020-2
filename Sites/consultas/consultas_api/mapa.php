9349598d4b7fd598ba68d82de17019785050b4cc
atroncoso2 <atroncoso2@uc.cl>
2020-12-08 17:31:51 -0300

    mapa(mapa): echo detalle de query

--- Sites/consultas/consultas_api/mapa.php
+++ Sites/consultas/consultas_api/mapa.php
@@ -157,13 +157,16 @@ $marker_list2 = [];
 
         echo "puertos: ";
         echo $puertos;
-        // echo "puertos[0]: ";
-        // echo $puertos[0];
+        echo "puertos[0]: ";
+        echo $puertos[0];
         // echo "puertos[0][0]: ";
         // echo $puertos[0][0];
 
         foreach ($puertos as $p) {
+            echo "p: ";
             echo $p;
+            echo "p[0]: ";
+            echo $p[0];
         }
 
     }
