a9a854272272c704637ceaa671111409dadbb95f
atroncoso2 <atroncoso2@uc.cl>
2020-12-08 18:21:58 -0300

    session(perfil): trabajo con sesiones

--- Sites/consultas/perfil.php
+++ Sites/consultas/perfil.php
@@ -1,7 +1,5 @@
 <?php session_start();?>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
-<?php include('../navbar.php'); ?>
-<?php include('../templates/header.html');   ?>
 <?php
 require("../config/conection.php");
 
@@ -43,6 +41,9 @@ echo $pasaporte;
 
 
 ?>
+
+<?php include('../navbar.php'); ?>
+<?php include('../templates/header.html');   ?>
 
 <?php
 if ($pasaporte == ""){
